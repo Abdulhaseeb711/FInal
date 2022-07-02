d1ef15d320833b8a667cb84d47bcb9f4ff11d4f1
Abdulhaseeb711 <108294099+Abdulhaseeb711@users.noreply.github.com>
2022-07-02 03:18:47 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require(`mongoose`)
+ const articleRouter = require(`./routes/articles`)
+const app = express()
+
+mongoose.connect(`mongodb+srv://abdulhaseeb:haseeb1234@cluster0.kymwvsy.mongodb.net/?retryWrites=true&w=majority`)
+
+app.set(`view engine`,`ejs`)
+
+app.use(`/articles`,articleRouter)
+//app.use(express.urlencoded({ extended: false }))
+
+app.get(`/`,(req,res)=>{
+    const articles =[{
+        title: `Test Article`,
+        createdAt : new Date(),
+        description: `Test Description`  
+    },
+    {
+        title: `Test Article 2`,
+        createdAt : new Date(),
+        description: `Test Description`  
+    }]
+     res.render(`articles/index.ejs`,{ articles: articles })
+})
+
+app.listen(5000,()=>{
+    console.log(`Server is running`);
+})
